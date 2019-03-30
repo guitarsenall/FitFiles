78005d7d3f946a3c5c6ca24c63b9625b8d1eecdd
Will Spicher <guitarsenall@gmail.com>
2019-03-30 09:28:07 -0500

    Include start-stop times in formatted print.

--- saddle_endurance_anls.py
+++ saddle_endurance_anls.py
@@ -72,9 +72,10 @@ if not all( s in signals.keys() for s in required_signals ):
         print >> OutStream, '   ' + s
     raise IOError(msg)
 
-SampleRate  = 1.0
-cadence     = signals['cadence']
-nPts        = len(cadence)
+SampleRate      = 1.0
+cadence         = signals['cadence']
+elapsed_time    = signals['time']
+nPts            = len(cadence)
 
 cad_fwd_min_3_8 = FwdRunningMinimum( cadence, wBeg=3, wEnd=8 )
 cad_fwd_min_1_8 = FwdRunningMinimum( cadence, wBeg=1, wEnd=8 )
@@ -101,23 +102,40 @@ for i in range(nPts):
 iiUp = np.nonzero( seated_state[1:] - seated_state[0:-1] ==  1 )[0]
 iiDn = np.nonzero( seated_state[1:] - seated_state[0:-1] == -1 )[0]
 if iiUp[-1] < iiDn[-1]:
-    seg_times   = np.zeros(len(iiDn))
+    seg_durtns  = np.zeros(len(iiDn))
+    seg_starts  = np.zeros(len(iiDn)).astype('int')
+    seg_stops   = np.zeros(len(iiDn)).astype('int')
 else:
-    seg_times   = np.zeros(len(iiDn)+1)
+    seg_durtns  = np.zeros(len(iiDn)+1)
+    seg_starts  = np.zeros(len(iiDn)+1).astype('int')
+    seg_stops   = np.zeros(len(iiDn)+1).astype('int')
 if iiDn[0] < iiUp[0]:                               # begins seated
-    seg_times[0]    = iiDn[0]
+    seg_durtns[0]   = iiDn[0]
+    seg_starts[0]   = 0
+    seg_stops [0]   = iiDn[0]
     if iiUp[-1] > iiDn[-1]:                         #   Ends seated
-        print '        ends seated'
-        seg_times[1:-1] = iiDn[1:] - iiUp[0:-1]
-        seg_times[-1]   = nPts - iiUp[-1]
+        seg_durtns[1:-1]    = iiDn[1:] - iiUp[0:-1]
+        seg_starts[1:-1]    = iiUp[0:-1]
+        seg_stops [1:-1]    = iiDn[1:]
+        seg_durtns[-1]  = nPts - iiUp[-1]
+        seg_starts[-1]  = iiUp[-1]
+        seg_stops [-1]  = nPts
     else:                                           #   ends standing
-        seg_times[1:]   = iiDn[1:] - iiUp
+        seg_durtns[1:]  = iiDn[1:] - iiUp
+        seg_starts[1:]  = iiUp
+        seg_stops [1:]  = iiDn[1:]
 elif iiUp[0] < iiDn[0]:                             # begins standing
     if iiUp[-1] > iiDn[-1]:                         #   ends seated
-        seg_times[0:-1] = iiDn - iiUp[0:-1]
-        seg_times[-1]   = nPts - iiUp[-1]
+        seg_durtns[0:-1]    = iiDn - iiUp[0:-1]
+        seg_starts[0:-1]    = iiUp[0:-1]
+        seg_stops [0:-1]    = iiDn
+        seg_durtns[-1]  = nPts - iiUp[-1]
+        seg_starts[-1]  = iiUp[-1]
+        seg_stops [-1]  = nPts
     else:                                           #   ends standing
-        seg_times = iiDn - iiUp
+        seg_durtns  = iiDn - iiUp
+        seg_starts  = iiUp
+        seg_stops   = iiDn
 else:
     raise RuntimeError("shouldn't be able to reach this code.")
 
@@ -148,13 +166,29 @@ print >> OutStream, 'standing time  : %2i:%02i:%02i (%2i%%))' % (hh, mm, ss, pct
 
 # segment results
 print >> OutStream, 'standing segments:'
-for i in range(len(seg_times)):
-    dur = seg_times[i]/SampleRate
-    hh  = dur // 3600
-    mm  = (dur % 3600) // 60
-    ss  = (dur % 3600) % 60
-    print >> OutStream, '    segment %2i: %2i:%02i:%02i' \
-                        % (i+1, hh, mm, ss)
+names   = [ 'segment', 'start', 'stop', 'duration' ]
+fmt     = "%12s"+"%10s"*3
+print >> OutStream, fmt % tuple(names)
+
+for i in range(len(seg_durtns)):
+    Beg = elapsed_time[ seg_starts[i] ]
+    hhBeg   =  Beg // 3600
+    mmBeg   = (Beg % 3600) // 60
+    ssBeg   = (Beg % 3600) % 60
+    End = elapsed_time[ seg_stops[i] ]
+    hhEnd   =  End // 3600
+    mmEnd   = (End % 3600) // 60
+    ssEnd   = (End % 3600) % 60
+    dur = seg_durtns[i]/SampleRate
+    hhDur   = dur // 3600
+    mmDur   = (dur % 3600) // 60
+    ssDur   = (dur % 3600) % 60
+    DurPlus = '' if hhDur==0 else '+%ih' % (hhDur)
+    fmt = '%12d'+'%4i:%02i:%02i'+'%4i:%02i:%02i' + '%7i:%02i' + '%s'
+    print >> OutStream, fmt \
+            % (i, hhBeg, mmBeg, ssBeg,
+                  hhEnd, mmEnd, ssEnd,
+                  mmDur, ssDur, DurPlus)
 
 ############################################################
 #                  plotting                                #
@@ -166,7 +200,7 @@ import matplotlib.dates as md
 from matplotlib.dates import date2num, DateFormatter
 import datetime as dt
 base = dt.datetime(2014, 1, 27, 0, 0, 0)
-x = [ base + dt.timedelta(seconds=t) for t in signals['time'] ]
+x = [ base + dt.timedelta(seconds=t) for t in elapsed_time ]
 x = date2num(x) # Convert to matplotlib format
 fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, sharex=True)
 ax0.plot_date( x, signals['power'], 'b-', linewidth=1 );
